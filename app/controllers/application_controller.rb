466fb092fe47cc95a115ade6b00728d940f36ba8
あなたの名前 <>
2025-07-28 01:43:52 +0900

    add: issue-3-ログインフォーム実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
-  # before_action :require_login
+before_action :require_login
+
+add_flash_types :success, :danger
 
   private
 
-  # def not_authenticated
-  #  redirect_to login_path
-  # end
+  def not_authenticated
+    redirect_to login_path, alert: 'ログインしてください'
+  end
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+  if @user
+    redirect_to home_path, success: 'ログインしました'
+  else
+    flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
+    render :new, status: :unprocessable_entity
+  end
+end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
+end
