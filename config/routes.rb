331364c1af9b0ddc554b033e0d746c642285fec3
あなたの名前 <>
2025-07-28 01:42:49 +0900

    add: issue-3-ホームページstatic_pagesへ移行

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#top"
 
-  get "home", to: "homes#top"
+  get 'home', to: 'static_pages#home'
 
   resources :users, only: %i[new create]
-
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
-  delete "login", to: "user_sessions#destroy"
+  delete "logout", to: "user_sessions#destroy"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
