14b55e9b5f254d23006b39519804c22c46449ee5
あなたの名前 <>
2025-07-29 01:01:40 +0900

    add: issue-12-エラーメッセージ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ add_flash_types :success, :danger
   private
 
   def not_authenticated
-    redirect_to login_path, alert: "ログインしてください"
+    redirect_to login_path, danger: "ログインしてください"
   end
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
