5e22a13c89aa6cf41824fc72d28eb270c4717621
あなたの名前 <>
2025-07-28 19:16:03 +0900

    add: issue-10-日記投稿一覧画面実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_26_172453) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_28_090314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "image"
+    t.integer "likes_count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -25,4 +36,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_172453) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
