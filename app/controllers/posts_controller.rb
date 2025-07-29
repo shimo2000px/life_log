e1253ca4297889e88cd231ae727ca0ccb4368426
あなたの名前 <>
2025-07-29 15:10:10 +0900

    add: issue-14-投稿書斎画面実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def post_params
