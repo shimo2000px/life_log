b59cb80dc6fad634d102ab8ceeeb46da09a4e73a
あなたの名前 <>
2025-07-31 11:23:49 +0900

    add: issue-22-diary_dateカラム追加し日付表示実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_29_121332) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_31_021553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_29_121332) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "diary_date", default: -> { "CURRENT_DATE" }
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
