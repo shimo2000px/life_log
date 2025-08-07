eebfffe061e20645ec4b2fb92fb3a9efcf116c60
あなたの名前 <>
2025-08-08 00:01:21 +0900

    add: issue-39-rubocop修正

--- app/controllers/admin/shared_posts_controller.rb
+++ app/controllers/admin/shared_posts_controller.rb
@@ -1,11 +1,11 @@
 class Admin::SharedPostsController < ApplicationController
   before_action :require_login
-  before_action :require_admin 
+  before_action :require_admin
 
   def index
     # is_sharedフラグを使った方が確実
     @shared_posts = Post.where(is_shared: true)
-                      .where.not(share_token: [nil, ""])
+                      .where.not(share_token: [ nil, "" ])
                       .includes(:user)
                       .order(created_at: :desc)
   end
@@ -14,9 +14,9 @@ class Admin::SharedPostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.shared?
       @post.destroy
-      redirect_to admin_shared_posts_path, notice: 'シェアされた投稿を削除しました', status: :see_other
+      redirect_to admin_shared_posts_path, notice: "シェアされた投稿を削除しました", status: :see_other
     else
-      redirect_to admin_shared_posts_path, alert: 'この投稿はシェアされていません'
+      redirect_to admin_shared_posts_path, alert: "この投稿はシェアされていません"
     end
   end
 
