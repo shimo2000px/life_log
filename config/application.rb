ba74afd576a26d145ce66c311c28a8b1549a4b68
あなたの名前 <>
2025-08-13 04:45:09 +0900

    add: デプロイ修正

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,5 @@
 require_relative "boot"
-
 require "rails/all"
-
-module Myapp
-  class Application < Rails::Application
-    config.load_defaults 8.0
-
-    # 日本時間
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :local
-
-    # 日本語をデフォルトロケールに設定
-    config.i18n.default_locale = :ja
-
-    # 使用可能なロケール
-    config.i18n.available_locales = [ :ja, :en ]
-
-    # ロケールファイルのパスを追加
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
-  end
-end
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -43,11 +22,25 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
     config.generators.system_tests = nil
     config.generators do |g|
       g.skip_routes true
       g.helper false
       g.test_framework nil
     end
+
+    # 日本時間
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    # 日本語をデフォルトロケールに設定
+    config.i18n.default_locale = :ja
+
+    # 使用可能なロケール
+    config.i18n.available_locales = [ :ja, :en ]
+
+    # ロケールファイルのパスを追加
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
   end
 end
