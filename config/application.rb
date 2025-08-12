40ba158e0fdddd90e60119a5cffaa23efc66982e
あなたの名前 <>
2025-08-13 04:47:40 +0900

    rubocop修正

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     config.generators.system_tests = nil
     config.generators do |g|
       g.skip_routes true
